d3995ae9c78ec7a99cda28f3d2ef663dfd77fe58
Sanidhya Dave <sanidhyad16@gmail.com>
2024-07-15 01:12:11 +0530

    minor report ctor change

--- Softmad.Services.LeadGeneration/Services/LeadGenerationService.cs
+++ Softmad.Services.LeadGeneration/Services/LeadGenerationService.cs
@@ -127,7 +127,10 @@ namespace Softmad.Services.LeadGeneration.Services
         public ReportResponse GetCurrentMonthReport()
         {
             _logger.LogDebug("Generating report");
-            var a = new ReportResponse(_leadRepository.GetCurrentMonthReport().ToList());
+            var a = new ReportResponse()
+            {
+                Response = _leadRepository.GetCurrentMonthReport().ToList()
+            };
             return a;
         }
 
--- Softmad.Services.Models/ReportResponse.cs
+++ Softmad.Services.Models/ReportResponse.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Softmad.Services.Models
+﻿namespace Softmad.Services.Models
 {
     public class ReportResponse
     {
-        public List<LeadsWithVists> Response { get; set; }
-        public ReportResponse(List<LeadsWithVists> leadswithvisits)
-        {
-            Console.WriteLine("Setting data leadswithvisits to response");
-            Response = leadswithvisits;
-        }
+        public List<LeadsWithVists> Response { get; set; }        
     }
 
     public class LeadsWithVists
